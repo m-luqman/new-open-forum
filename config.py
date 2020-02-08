01744f6f3b396641545dbb64719cc5ba83ca5d92
muhammadluqman <muhammadluqman@192.168.0.110>
2020-02-08 14:10:36 +0530

    Secured the config file

--- config.py
+++ config.py
@@ -3,9 +3,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secretkey-we-can-set'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    GOOGLE_CLIENT_ID = "530958200715-qau4bjsgpu9dfn3h4gtvokekr41fp950.apps.googleusercontent.com"
-    GOOGLE_CLIENT_SECRET = "OM46KDb7CqbdeTRsX7be_xg1"
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    GOOGLE_CLIENT_SECRET=os.environ.get("GOOGLE_CLIENT_SECRET")
+    GOOGLE_CLIENT_ID=os.environ.get("GOOGLE_CLIENT_ID")
     GOOGLE_DISCOVERY_URL = ("https://accounts.google.com/.well-known/openid-configuration")
